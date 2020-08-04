3fdb8af620a494ce2e06c138791034a5cef9b055
dmackca <dmack.ca@gmail.com>
2020-08-03 22:13:26 -0700

    update db seeding script

--- src/http/get-s33d/index.js
+++ src/http/get-s33d/index.js
@@ -7,12 +7,47 @@ exports.handler = async function http (req) {
     {
         filter: '^Perry.Mason.+1080p',
         latestSeason: 1,
-        latestEpisode: 5,
+        latestEpisode: 7,
     },
     {
         filter: '^12.oz.Mouse.+1080p',
         latestSeason: 3,
         latestEpisode: 1,
+    },
+    {
+        filter: '^Better.Call.Saul.+1080p',
+        latestSeason: 5,
+        latestEpisode: 10,
+    },
+    {
+        filter: '^Its.Always.Sunny.in.Philadelphia.+1080p',
+        latestSeason: 14,
+        latestEpisode: 10,
+    },
+    {
+        filter: '^Kidding.+1080p',
+        latestSeason: 2,
+        latestEpisode: 10,
+    },
+    {
+        filter: '^on.becoming.a.god.in.central.florida.+1080p',
+        latestSeason: 1,
+        latestEpisode: 10,
+    },
+    {
+        filter: '^Genndy.Tartakovskys.Primal.+1080p',
+        latestSeason: 1,
+        latestEpisode: 6,
+    },
+    {
+        filter: '^Rick.+Morty.+1080p',
+        latestSeason: 4,
+        latestEpisode: 10,
+    },
+    {
+        filter: '^Snowpiercer.+1080p',
+        latestSeason: 1,
+        latestEpisode: 10,
     },
   ];
 
