00ee5a466ef0362d072ba6cc5ba76295f25e6eff
dmackca <dmack.ca@gmail.com>
2020-12-13 19:56:53 -0800

    add AoT subscription
    add missing "keys" from dev sandbox data

--- scripts/sandbox-startup.js
+++ scripts/sandbox-startup.js
@@ -19,26 +19,45 @@ async function startUpScript() {
     const subscriptions = [
         {
             filter: '^Hotel.Instantane.+1080p',
+            key: '^Hotel.Instantane.+1080p',
             latestSeason: 2,
             latestEpisode: 5,
         },
         {
+            filter: '^Attack.on.Titan.+1080p',
+            key: 'Attack.on.Titan',
+            feed: 'AB',
+            latestSeason: 0,
+            latestEpisode: 59,
+        },
+        {
             filter: 'Saturday Morning with James Martin.+1080p',
+            key: 'Saturday Morning with James Martin.+1080p',
             latestSeason: 0,
             latestEpisode: 0,
         },
         {
             filter: 'Some show that wont be matched.+1080p',
+            key: 'Some show that wont be matched.+1080p',
             latestSeason: 2,
             latestEpisode: 1,
         },
         {
+            filter: 'Healin.+Good.+1080p',
+            key: 'test healin good.+1080p',
+            feed: 'AB',
+            latestSeason: 0,
+            latestEpisode: 31,
+        },
+        {
             filter: 'Pit Bulls and Parolees.+1080p',
+            key: 'Pit Bulls and Parolees.+1080p',
             latestSeason: 0,
             latestEpisode: 0,
         },
         {
             filter: 'Corn Pone Wisdom.+1080p',
+            key: 'Corn Pone Wisdom.+1080p',
             latestSeason: 0,
             latestEpisode: 0,
         },
